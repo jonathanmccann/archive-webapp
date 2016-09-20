5cdba84104faa7a3bed2669dba061be0c2f5341b
Jonathan McCann <jonathan.mccann@liferay.com>
2016-09-20 13:30:36 -0700

    Set up simple MVC application

---
+++ src/java/com/archive/controller/ArchiveController.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2016-present Jonathan McCann
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ */
+
+package com.archive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Jonathan McCann
+ */
+@Controller
+public class ArchiveController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+
+}
